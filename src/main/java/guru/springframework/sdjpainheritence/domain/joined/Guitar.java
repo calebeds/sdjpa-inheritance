9968081abf51a00e5b2b71fb911f6b821c67b92f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-02-26 18:17:57 -0300

    add mapped joined table example

---
+++ src/main/java/guru/springframework/sdjpainheritence/domain/joined/Guitar.java
@@ -0,0 +1,16 @@
+package guru.springframework.sdjpainheritence.domain.joined;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Guitar extends Instrument {
+    private Integer numberOfStrings;
+
+    public Integer getNumberOfStrings() {
+        return numberOfStrings;
+    }
+
+    public void setNumberOfStrings(Integer numberOfStrings) {
+        this.numberOfStrings = numberOfStrings;
+    }
+}
